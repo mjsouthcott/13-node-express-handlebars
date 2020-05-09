5185f84ee925bb8b2312af3362b73e1fdb3539b8
Matthew Southcott <mjsouthcott@gmail.com>
2020-05-09 09:06:10 -0400

    Update ORM

--- config/orm.js
+++ config/orm.js
@@ -3,21 +3,21 @@ const connection = require("./connection");
 
 // Define orm object
 const orm = {
-  // Define selectAll() method
+  // Define selectAll method
   selectAll: async () => {
     const sql = "SELECT * FROM burgers;";
     const [rows] = await connection.query(sql);
     return rows;
   },
 
-  // Define insertOne() method
+  // Define insertOne method
   insertOne: async (burgerName, isDevoured) => {
     const sql = "INSERT INTO burgers (burger_name, devoured) VALUES (?, ?);";
     const [rows] = await connection.query(sql, [burgerName, isDevoured])
     return rows;
   },
 
-  // Define updateOne() method
+  // Define updateOne method
   updateOne: async (burger_name) => {
     const sql = "UPDATE burgers SET `devoured` = 1 WHERE burger_name = ?";
     const [rows] = await connection.query(sql, burger_name)
